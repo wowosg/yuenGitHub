dba9be3891dba690727af8f1de4e7bff7f9435e5
andycheng <andychz0519@gmail.com>
2020-10-05 16:26:53 +0800

    Initial Commit

---
+++ yuen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yuen
+//
+//  Created by User12 on 2020/10/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ yuen/yuenApp.swift
@@ -0,0 +1,17 @@
+//
+//  yuenApp.swift
+//  yuen
+//
+//  Created by User12 on 2020/10/5.
+//
+
+import SwiftUI
+
+@main
+struct yuenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
